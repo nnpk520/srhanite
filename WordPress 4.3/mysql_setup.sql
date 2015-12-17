c82ad9406a42ca401186289f44a03b128973a63e
simonhutson <simonhutson@users.noreply.github.com>
2015-12-17 07:31:12 +0000

    Delete mysql_setup.sql

--- WordPress 4.3/mysql_setup.sql
+++
@@ -1,4 +0,0 @@
-SET PASSWORD for 'root'@'localhost' = PASSWORD('Pass@word1');
-CREATE DATABASE wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost' IDENTIFIED BY 'Pass@word1';
-DROP DATABASE test;
