dbc2830dd2bc38da4e13fa8de347908a39af49e7
simonhutson <simonhutson@users.noreply.github.com>
2016-01-19 00:41:46 +0000

    Create docker-entrypoint.sh

---
+++ CentOS-7.1-Apache/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+set -e
+
+if [ "$1" = 'httpd' ]; then
+
+  if [ "$2" = '-DFOREGROUND' ]; then
+  
+    # Delete temporary httpd files from an incomplete shutdown
+    rm -rf /run/httpd/* /tmp/httpd*
+    
+  fi
+  
+fi
+
+exec "$@"
