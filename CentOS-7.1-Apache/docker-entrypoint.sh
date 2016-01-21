f69b1fe82efa45438567e438a0b4227cfbc27a62
simonhutson <simonhutson@users.noreply.github.com>
2016-01-21 10:05:25 +0000

    Update docker-entrypoint.sh

--- CentOS-7.1-Apache/docker-entrypoint.sh
+++ CentOS-7.1-Apache/docker-entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/bash 
 set -e
 
-if [ "$1" = 'httpd' ]; then
-
-  if [ "$2" = '-DFOREGROUND' ]; then
+if [ "$1" = 'httpd' -a "$2" = '-DFOREGROUND' ]; then
   
     # Delete temporary httpd files from an incomplete shutdown
     rm -rf /run/httpd/* /tmp/httpd*
-    
-  fi
   
 fi
 
